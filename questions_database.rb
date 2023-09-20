a79fda7e80761978da4bf312f14bb63b1fb1af46
JacobSeidner <jseidne1@binghamton.edu>
2023-09-20 16:17:12 -0400

    doing great

--- questions_database.rb
+++ questions_database.rb
@@ -1,12 +1,59 @@
 require 'sqlite3'
 require 'singleton'
 
+class QuestionsDatabase < SQLite3::Database
+    include Singleton
+    def initialize
+        super('questions.db')
+        self.type_translation = true
+        self.results_as_hash = true
+    end
+end
 
+class Question
+        attr_accessor :id, :title, :body, :user_id
 
-class QuestionsDatabase < SQLite3::Database 
-    include Singleton 
+    def self.find_by_id(id)
+        question = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT
+        *
+        FROM
+        questions
+        WHERE
+        id = ?
+        SQL
+        return nil unless question.length > 0
+        Question.new(question.first)
+    end
 
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @user_id = options['user_id']
+    end
+end
 
+class User
+    attr_accessor :id, :f_name, :l_name
 
+    def self.find_by_name(f_name, l_name)
+        user = QuestionsDatabase.instance.execute(<<-SQL, f_name, l_name)
+        SELECT
+        *
+        FROM
+        users
+        WHERE
+        f_name = ?
+        l_name = ?
+        SQL
+        return nil unless user.length > 0
+        User.new(user.first)
+    end
 
-end 
+    def initialize(options)
+        @id = options['id']
+        @f_name = options['f_name']
+        @l_name = options['l_name']
+    end
+end
